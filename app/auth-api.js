5e68ad41e2c06f498708186b2985de74d005eb1b
vfcheung <vfhcheung@gmail.com>
2019-05-20 16:33:45 -0700

    Refactor auth-api.js

--- app/auth-api.js
+++ app/auth-api.js
@@ -23,125 +23,76 @@ function dummy_generate_cs(up, little_a, big_a, big_b, user_salt) {
   return {m1: 5, m2: 6}
 }
 
+function postData(url = '', data = {}) {
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer',
+    body: JSON.stringify(data),
+  })
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok.');
+  }).then(function(json) {
+    return(json);
+  }).catch(function(error) {
+    console.log('Fetch error: ', error.message);
+    return null;
+  })
+}
+
 function register(username, password) {
   rvals = dummy_generate_registration(username + password);
-  return fetch(routes.register, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify({
-      'username': username,
-      'user_salt': rvals.s,
-      'user_verifier': rvals.v
-    })
-  }).then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Network response was not ok.');
-  }).then(function(json) {
-    console.log(json);
-    return(json);
-  }).catch(function(err) {
-    console.log('Fetch error: ', err.message);
-    return null;
+  return postData(routes.register, {
+    'username': username,
+    'user_salt': rvals.s,
+    'user_verifier': rvals.v
   })
 }
 
-function get_salt(username, password) {
-  return fetch(routes.login.get_salt, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify({
-      'username': username,
-    })
-  }).then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Network response was not ok.');
-  }).then(function(json) {
-    return get_b(username, password, json.user_salt, json.nonce);
-  }).catch(function(err) {
-    console.log('Fetch error: ', err.message);
-    return null;
+function get_salt(username) {
+  return postData(routes.login.get_salt, {
+    'username': username
   })
 }
 
-function get_b(username, password, user_salt, nonce) {
-  avals = dummy_generate_a();
-  return fetch(routes.login.get_b, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify({
-      'big_a': avals.big_a,
-      'nonce': nonce,
-    })
-  }).then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Network response was not ok.');
-  }).then(function(json) {
-    return get_m2(username, password, avals.little_a, avals.big_a, json.big_b, user_salt, nonce)
-  }).catch(function(err) {
-    console.log('Fetch error: ', err.message);
-    return null;
+function get_b(big_a, nonce) {
+  return postData(routes.login.get_b, {
+    'big_a': big_a,
+    'nonce': nonce,
   })
 }
 
-function get_m2(username, password, little_a, big_a, big_b, user_salt, nonce) {
-  vvals = dummy_generate_cs(username + password, little_a, big_a, big_b, user_salt);
-  return fetch(routes.login.get_m2, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify({
-      'm1': vvals.m1,
-      'nonce': nonce,
-    })
-  }).then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error('Network response was not ok.');
-  }).then(function(json) {
-    console.log(json);
-    return(json);
-  }).catch(function(err) {
-    console.log('Fetch error: ', err.message);
-    return null;
+function get_m2(m1, nonce) {
+  return postData(routes.login.get_m2, {
+    'm1': m1,
+    'nonce': nonce,
   })
 }
 
 function login(username, password) {
-  get_salt(username, password)
+  get_salt(username)
+  .then(function(res1) {
+    console.log(res1);
+    avals = dummy_generate_a();
+    get_b(avals.big_a, res1.nonce)
+    .then(function(res2) {
+      console.log(res2);
+      vvals = dummy_generate_cs(username + password, avals.little_a, avals.big_a, res2.big_b, res1.user_salt);
+      get_m2(vvals.m1, res1.nonce)
+      .then(function(res3) {
+        console.log(res3)
+      })
+    })
+  })
 }
 
 register('username', 'password');
