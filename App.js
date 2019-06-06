9969ac778fdbc84b44b4cce9834a5eaa0b982b27
Brian Chan <brianc725@gmail.com>
2019-06-06 11:39:09 -0700

    Fixed merge conflict with sound in notification by Spenser

--- App.js
+++ App.js
@@ -7,107 +7,166 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Alert } from 'react-native';
+// import { Platform, StyleSheet, Text, View } from 'react-native';
+// import Login from './app/Screens/Login';
+// import StockList from './app/Screens/StockList';
+// import Register from './app/Screens/Register';
+
+import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import Login from './app/Screens/Login';
+import StockList from './app/Screens/StockList';
+import Register from './app/Screens/Register';
 import Navigator from './app/Navigator';
 import AsyncStorage from '@react-native-community/async-storage';
 import firebase from 'react-native-firebase';
+import Sound from 'react-native-sound';
+
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 type Props = {};
 export default class App extends Component<Props> {
-  async componentDidMount() {
-    this.checkPermission();
-    this.createNotificationListeners(); //add this line
-  }
+async componentDidMount() {
+	const channel = new firebase.notifications.Android.Channel(
+	"notification",
+	"Notification",
+	firebase.notifications.Android.Importance.High,
+	)//.setSound("default");
+    firebase.notifications().android.createChannel(channel);
+  this.checkPermission();
+  this.createNotificationListeners(); 
+  
+}
 
   //1
-  async checkPermission() {
-    const enabled = await firebase.messaging().hasPermission();
-    if (enabled) {
+async checkPermission() {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
       this.getToken();
-    } else {
+  } else {
       this.requestPermission();
-    }
   }
+}
 
   //3
-  async getToken() {
-    let fcmToken = await AsyncStorage.getItem('fcmToken');
-    if (!fcmToken) {
+async getToken() {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  if (!fcmToken) {
       fcmToken = await firebase.messaging().getToken();
       if (fcmToken) {
-        // user has a device token
-        await AsyncStorage.setItem('fcmToken', fcmToken);
+          // user has a device token
+          await AsyncStorage.setItem('fcmToken', fcmToken);
       }
-    }
   }
+    console.log('Token:' + fcmToken);
+
+}
 
   //2
-  async requestPermission() {
-    try {
+async requestPermission() {
+  try {
       await firebase.messaging().requestPermission();
-      console.log('Check Permission')
+        console.log('Check Permission')
       // User has authorised
       this.getToken();
-    } catch (error) {
+  } catch (error) {
       // User has rejected permissions
       console.log('permission rejected');
-    }
   }
+}
 
-  componentWillUnmount() {
-    this.notificationListener();
-    this.notificationOpenedListener();
-  }
+componentWillUnmount() {
+  this.notificationListener();
+  this.notificationOpenedListener();
+}
 
-  async createNotificationListeners() {
-    /*
-    * Triggered when a particular notification has been received in foreground
-    * */
-    this.notificationListener = firebase.notifications().onNotification((notification) => {
-      console.log('I was triggered')
-      const { title, body } = notification;
-      this.showAlert(title, body);
-    });
+async createNotificationListeners() {
+  /*
+  * Triggered when a particular notification has been received in foreground
+  * */
+  this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const {title, body} = notification;
+      this.sendNotification(notification.setSound("default"));
+  });
 
-    /*
-    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
-    * */
-    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
-      const { title, body } = notificationOpen.notification;
-      this.showAlert(title, body);
-    });
+  /*
+  * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+  * */
+  this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+  	  this.sendNotification(notificationOpen.notification.setSound("default"));
+  	    });
 
-    /*
-    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
-    * */
-    const notificationOpen = await firebase.notifications().getInitialNotification();
-    if (notificationOpen) {
-      const { title, body } = notificationOpen.notification;
-      this.showAlert(title, body);
-    }
-    /*
-    * Triggered for data only payload in foreground
-    * */
-    this.messageListener = firebase.messaging().onMessage((message) => {
-      //process data message
-      console.log(JSON.stringify(message));
-    });
-  }
+  /*
+  * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+  * */
+  const notificationOpen = await firebase.notifications().getInitialNotification();
+  // if (notificationOpen) {
+  // 	 this.sendNotification(notificationOpen.notification.setSound("default"));
+  // }
+  /*
+  * Triggered for data only payload in foreground
+  * */
+  this.messageListener = firebase.messaging().onMessage((message) => {
+    //process data message
+    console.log(JSON.stringify(message));
+  });
+}
 
-  showAlert(title, body) {
-    Alert.alert(
-      title, body,
-      [
-        { text: 'OK', onPress: () => console.log('OK Pressed') },
-      ],
-      { cancelable: false },
-    );
-  }
-
+sendNotification(notification){
+	var sound = new Sound('cha_ching_sound', Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+                console.log('failed to load the sound', error);
+            } else {
+                sound.play(); // have to put the call to play() in the onload callback
+            }
+        });
+	const channelGroup = new firebase.notifications.Android.ChannelGroup('notification-group', 'Notification Group');
+	firebase.notifications().android.createChannelGroup(channelGroup);
+      notification = notification//.setSound("default")
+      .android.setPriority(firebase.notifications.Android.Priority.Max)
+      .android.setChannelId('notification')
+      .android.setGroup('notification-group')
+      .android.setGroupSummary(true)
+      .android.setGroupAlertBehaviour(firebase.notifications.Android.GroupAlert.All)
+      .android.setCategory(firebase.notifications.Android.Category.Alarm)
+      .android.setPriority(firebase.notifications.Android.Priority.High)
+      .android.setAutoCancel(true);
+      firebase.notifications().displayNotification(notification);
+}
 
   render() {
     return (
-      <Navigator />
+      // <Login></Login>
+      // <Register></Register>
+      // <StockList></StockList>
+      // <SignedOut></SignedOut>
+      // <SignedIn/>
+      <Navigator/>
     );
   }
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
--- android/app/src/main/java/com/stocknotifier/MainApplication.java
+++ android/app/src/main/java/com/stocknotifier/MainApplication.java
@@ -3,6 +3,7 @@ package com.stocknotifier;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.horcrux.svg.SvgPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
             new RNCWebViewPackage(),
 	        new ClientLoginPackage(),
           new SvgPackage(),
